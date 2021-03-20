a1613874ace47055bafd9b996904e8ceaac22ccb
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2021-03-20 18:59:07 +0300

    njoud's first push

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Congrats!! Rana was here");
+        myRef.setValue("Congrats!! Njoud was here");
     }
 }
